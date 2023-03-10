65900aa4f0a3048db9cd492b77b11dacd2b665cb
shraaavss <shravs2707@gmail.com>
2023-03-10 11:40:54 +0530

    added some confetti

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,17 +1,30 @@
 // For the greeting box and the box itself
 import React from "react";
 import Buttons from "./Buttons";
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
 
 export default function Box(props){
 
 const [isStupid, setIsStupid] = React.useState(false);
-
+const { width, height } = useWindowSize()
 
     return(
         <div className="bigcontainer">
             <h1 className="question">{
                 isStupid?"It's okay, everyone is! <3":`Are you an idiot, ${props.name}? uwu`
             }</h1>
+
+
+            {isStupid&&<div>
+                <Confetti
+                width={width}
+                height={height}
+                numberOfPieces={150}
+              />
+            </div>
+            }
+
             {!isStupid&&<div className="answer">
                 <Buttons text="yes, I am" onClick={()=> setIsStupid(true)} id="yes"/>
                 <Buttons text="no, I'm not" id="no"/>
